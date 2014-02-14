abb85287664303413e93898615b448ed7de29aa6
Amir Arbel <amirarbel@gmail.com>
2014-02-14 16:20:38 +0200

    Rough draft.

---
+++ firstTry/firstTry.ino
@@ -0,0 +1,29 @@
+// Setup variables, input pin numbers, etc.
+
+void setup()
+{
+  // Should setup a button and a light/uv sensor.
+  // Also a thermometer (on the inside on specific room).
+}
+
+void loop()
+{
+  // Check if there's light out.
+  // If there is, check to what temperature the system is set to.
+  // if(temp < optimalTemp && !windowOpen)
+    // open();
+  //}
+  // if(temp > optimalTemp && windowOpen) {
+    // close();
+  //}
+}
+
+// void close() {
+  //close windows.
+  //windowOpen = false;
+//}
+
+// void open() {
+  //open windows.
+  //windowOpen = true;
+//}
